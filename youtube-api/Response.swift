1919d728530625abd40576d3ca4ea5bbdbfb5b35
michaeloconn98 <michael.oconn@hotmail.co.uk>
2020-08-31 18:11:32 +0100

    added code to parse the json response

---
+++ youtube-api/Response.swift
@@ -0,0 +1,28 @@
+//
+//  Response.swift
+//  youtube-api
+//
+//  Created by Michael O'Connor on 31/08/2020.
+//  Copyright © 2020 Michael O'Connor. All rights reserved.
+//
+
+import Foundation
+
+struct Response: Decodable {
+    
+    var items:[Video]?
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case items
+        
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.items = try container.decode([Video].self, forKey: .items)
+    }
+    
+}
