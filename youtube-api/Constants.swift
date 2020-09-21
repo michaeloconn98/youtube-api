34189674e0ea3f18ea1cb9d9f62d8bf1d979f376
michaeloconn98 <michael.oconn@hotmail.co.uk>
2020-09-21 15:45:49 +0100

    Upto Data version

--- youtube-api/Constants.swift
+++ youtube-api/Constants.swift
@@ -11,7 +11,7 @@ import Foundation
 struct Constants {
     
     static var API_KEY = ""
-    static var PLAYLIST_ID = "UU2D6eRvCeMtcF5OGHf1-trw"
+    static var PLAYLIST_ID = "UUU2PacFf99vhb3hNiYDmxww"
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     static var VIDEOCELL_ID = "VideoCell"
     static var YT_EMBED_URL = "https://www.youtube.com/embed/"
